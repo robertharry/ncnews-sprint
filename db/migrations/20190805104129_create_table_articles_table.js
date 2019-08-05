2ba117021add131f33397df51a8ad5cdebea25e1
Robert Harrison <robertharrison@Roberts-MacBook-Air.local>
2019-08-05 14:29:48 +0100

    seed function completed

--- db/migrations/20190805104129_create_table_articles_table.js
+++ db/migrations/20190805104129_create_table_articles_table.js
@@ -4,7 +4,7 @@ exports.up = function(knex) {
   return knex.schema.createTable('articles', (articleTable) => {
       articleTable.increments('article_id').primary();
       articleTable.string('title');
-      articleTable.string('body');
+      articleTable.text('body');
       articleTable.integer('votes').defaultTo(0);
       articleTable.string('topic').references('topics.slug');
       articleTable.string('author').references('users.username');
