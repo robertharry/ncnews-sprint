962da371f317357ff93d9b4ba21c311a643567a4
Robert Harrison <robertharrison@Roberts-MacBook-Air.local>
2019-08-23 14:48:29 +0100

    added cors

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const apiRouter = require('./routers/api-router');
 const { customErrors, psqlErrors, otherErrors, send405Error} = require('./errors/index')
+const cors = require('cors');
 app.use(express.json());
+app.use(cors());
 
 app.use('/api', apiRouter).all(send405Error)
 
